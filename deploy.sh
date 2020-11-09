ad92dd6035feeb78d79b8df4b3daab47b2cfd290
Гвозди Заржавели <annuananipon@njs.netlab.cz>
2020-11-10 01:14:23 +0300

    wip

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ git pull
 cd ../dive-site-russia-deploy
 
 docker-compose up -d --build --no-deps ${1}
+
+if [ "$1" = "dive-site-russia-api" ]; then
+    docker-compose exec -T dive-site-russia-api php artisan migrate
+fi
